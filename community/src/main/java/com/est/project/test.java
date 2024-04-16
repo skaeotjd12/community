7be98ec5bf3a505f729547c1a72f7ed39f925e24
DESKTOP-AN1016U\skaeo <skaeotjd0@gmail.com>
2024-04-16 17:14:00 +0900

    sdsd

---
+++ community/src/main/java/com/est/project/test.java
@@ -0,0 +1,16 @@
+package com.est.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+	
+	@GetMapping
+	public String name() {
+		return "get";
+	}
+	
+}
+
+
